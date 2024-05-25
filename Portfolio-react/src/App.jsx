ea747ae16d670c09fda0ee5876afafda67420392
waibaBikash <kavrelibikash@gmail.com>
2024-05-26 08:15:01 +1000

    added my services

--- Portfolio-react/src/App.jsx
+++ Portfolio-react/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   )
 }
