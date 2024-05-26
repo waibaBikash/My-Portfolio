11f6f548dcb3fdbb969e27046634cf5838f554f4
waibaBikash <kavrelibikash@gmail.com>
2024-05-26 18:16:50 +1000

    create footer

--- Portfolio-react/src/App.jsx
+++ Portfolio-react/src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Services from './components/Services/Services';
 import MyWork from './components/MyWork/MyWork';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Services />
       <MyWork />
       <Contact />
+      <Footer />
     </div>
   )
 }
