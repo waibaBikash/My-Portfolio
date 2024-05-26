50b9730a4be7121b61d687fe42452d8a3d835841
waibaBikash <kavrelibikash@gmail.com>
2024-05-26 21:18:10 +1000

    onSubmit functionality

--- Portfolio-react/src/components/Contact/Contact.jsx
+++ Portfolio-react/src/components/Contact/Contact.jsx
@@ -6,8 +6,31 @@ import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bed04237-5dbf-4551-981d-bab8dd3e0f7d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <div className='contact'>
+    <div id='contact' className='contact'>
        <div className="contact-title">
           <h1>Get in touch</h1>
           <img src={theme_pattern} alt="" />
@@ -31,7 +54,7 @@ const Contact = () => {
                 </div>
              </div>
            </div>
-             <form className='contact-right'>
+             <form onSubmit={onSubmit} className='contact-right'>
               <label htmlFor="">Your Name</label>
               <input type="text" placeholder='Enter your name' name='name' />
               <label htmlFor="">Your Email</label>
--- Portfolio-react/src/components/Hero/Hero.jsx
+++ Portfolio-react/src/components/Hero/Hero.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Hero.css';
 import porfile_img from '../../assets/profile_img.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
        <img src={porfile_img} alt="" />
        <h1><span>I'm Bikash Lama, </span>developer based in Australia</h1>
        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque deleniti qui pariatur possimus aut. Mollitia libero eos ipsam non minus voluptas similique veritatis facilis temporibus possimus exercitationem, asperiores consequatur reprehenderit.</p>
        <div className="hero-action">
-          <div className="hero-connect">Connect with me</div>
+          <div className="hero-connect"> <AnchorLink className='anchor-link' offset={50} href="#contact">Connect with me </AnchorLink>
+          </div>
           <div className="hero-resume">My resume</div>
        </div>
     </div>
