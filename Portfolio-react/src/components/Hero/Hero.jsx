a4b947781d3bff75ea126f4da739294e94430b23
waibaBikash <kavrelibikash@gmail.com>
2024-06-05 21:32:53 +1000

    changes made

--- Portfolio-react/src/components/Hero/Hero.jsx
+++ Portfolio-react/src/components/Hero/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Hero.css';
-import porfile_img from '../../assets/profile_img.svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-       <img src={porfile_img} alt="" />
-       <h1><span>I'm Bikash Lama Waiba, </span>Computer Engineer Full Stack developer based in Australia</h1>
-       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque deleniti qui pariatur possimus aut. Mollitia libero eos ipsam non minus voluptas similique veritatis facilis temporibus possimus exercitationem, asperiores consequatur reprehenderit.</p>
+       <img src="../../public/about-me.png" alt="" />
+       <h1><span>I'm Bikash Lama Waiba, </span>Web developer based in Australia</h1>
+       <p>Dynamic and resourceful Full Stack Developer with two years of hands-on experience in designing, developing, and implementing web applications. Proficient in both front-end and back-end technologies, adept at collaborating with cross-functional teams to deliver high-quality solutions. Demonstrated expertise in utilizing a diverse range of programming languages, frameworks, and tools to streamline processes and enhance user experiences. Possesses a keen eye for detail, a passion for innovation, and a commitment to staying updated with emerging technologies in the ever-evolving landscape of web development.
+      </p>
        <div className="hero-action">
           <div className="hero-connect"> <AnchorLink className='anchor-link' offset={50} href="#contact">Connect with me </AnchorLink>
           </div>
